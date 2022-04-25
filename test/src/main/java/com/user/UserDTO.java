40afe269e3cd805d39533ae40c5a5af222bfb316
82109 <82109@61.72.208.1>
2022-04-25 15:10:52 +0900

    두번째 테스트

--- test/src/main/java/com/user/UserDTO.java
+++ test/src/main/java/com/user/UserDTO.java
@@ -2,6 +2,7 @@ package com.user;
 
 public class UserDTO {
 	private String userId;
+	private String userName;
 
 	public String getUserId() {
 		return userId;
@@ -10,5 +11,14 @@ public class UserDTO {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 
 }
